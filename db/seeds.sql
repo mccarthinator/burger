6dd56b34de6c7ecf9cece1f099543ed1e9cdb86b
mccarthinator <mccarthy.cath@gmail.com>
2017-08-18 17:24:48 -0500

    seeds.sql data

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name, devoured, time_stamp) VALUES
+('1', 'the tastiest burg', false, '2003-04-05 00:02:05'),
+('2', 'this burg is just ok', false, '2016-01-24 06:07:23'),
+('3', 'this burg made me sick', false, '2017-08-18 12:01:10'),
