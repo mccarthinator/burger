c5770feabe8f89180abc7d9ecbcf60a8817bec93
mccarthinator <mccarthy.cath@gmail.com>
2017-08-18 17:04:15 -0500

    setting up server.js

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-// Require the following npm packages inside of the server.js file:
-// express
-// method-override
-// body-parser
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+//serve static
+app.use(express.static("public"));
+//Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller");
+
+app.use('/', routes);
+
+app.listen(PORT, function(){
+    console.log(`Da Burger is running on PORT ${PORT}`);
+});
