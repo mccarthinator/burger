7c039dcb8a59586a588a3d2f874c411158eaee40
mccarthinator <mccarthy.cath@gmail.com>
2017-09-19 08:45:10 -0500

    server.js changes

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
-var port = process.env.PORT || 3000;
-
+var PORT = process.env.PORT || 3000;
 var app = express();
-
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
+// override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
-
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgersController.js");
 
 app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
-app.listen(port);
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
